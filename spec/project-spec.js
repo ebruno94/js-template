234e2a13a36c6651e31257bd4f5c890ce396563f
Ernest <ernest.bruno94@yahoo.com>
2018-03-14 15:04:37 -0700

    Add babel and configure necessary files.

---
+++ spec/project-spec.js
@@ -0,0 +1,7 @@
+import {project class} from "project path";
+
+describe('Class we want to describe', function() {
+  it('should test that this really does that', function(){
+      //Test content goes here;
+  });
+});
